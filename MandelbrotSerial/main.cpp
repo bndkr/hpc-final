13d45c07e4c4257406eb13ebf9ca67188985080e
bndkr <bend@duck.com>
2023-11-07 15:53:44 -0700

    add MandelbrotSerial project

---
+++ MandelbrotSerial/main.cpp
@@ -0,0 +1,80 @@
+#include "bmp.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace
+{
+    double calculatePixel(const double x_0, const double y_0, const unsigned maxIterations)
+    {
+        // cardioid check
+        double p = sqrt((x_0 - 0.25) * (x_0 - 0.25) + y_0 * y_0);
+        if (x_0 <= p - (2 * p * p) + 0.25)
+            return -1;
+
+        // period 2 bulb check
+        if ((x_0 + 1) * (x_0 + 1) + y_0 * y_0 <= 1.0 / 16)
+            return -1;
+
+        double z_x = 0;
+        double z_y = 0;
+
+        double x_2 = 0;
+        double y_2 = 0;
+
+        unsigned int iteration = 0;
+
+        while (x_2 + y_2 < 4 && iteration < maxIterations)
+        {
+            // iterate: z = z^2 + c
+            z_y = 2 * z_x * z_y + y_0;
+            z_x = x_2 - y_2 + x_0;
+            x_2 = z_x * z_x;
+            y_2 = z_y * z_y;
+            iteration++;
+        }
+        if (iteration == maxIterations)
+        {
+            return -1; // inside the mandelbrot set
+        }
+        return iteration;
+    }
+} // namespace
+
+#define HEIGHT 200
+#define WIDTH 200
+
+#define BLACK 0
+#define WHITE 255
+
+int main(int, char**)
+{
+    unsigned char* pImage = (unsigned char*)malloc(HEIGHT * WIDTH * BYTES_PER_PIXEL);
+    char* imageFileName = (char*)"bitmapImage.bmp";
+
+    int i, j;
+    for (i = 0; i < HEIGHT; i++)
+    {
+        for (j = 0; j < WIDTH; j++)
+        {
+            double iterations = calculatePixel(-2.0 + (j * 4.0 / WIDTH), (2.0 - (i * 4.0 / HEIGHT)), 100);
+            if (iterations == -1)
+            {
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 2] = BLACK; // red
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 1] = BLACK; // green
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 0] = BLACK; // blue
+            }
+            else
+            {
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 2] = WHITE; // red
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 1] = WHITE; // green
+                pImage[i * WIDTH * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL + 0] = WHITE; // blue
+            }
+        }
+    }
+
+    generateBitmapImage(pImage, HEIGHT, WIDTH, imageFileName);
+    printf("Image generated!!");
+    return 0;
+}
