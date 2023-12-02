bce5752aba9742f2b2b800e7ed22c5308d4b060c
bndkr <bend@duck.com>
2023-12-01 17:18:25 -0700

    fix extern 'C' guards

--- computePixel.h
+++ computePixel.h
@@ -1,9 +1,12 @@
 #ifndef COMPUTEPIXEL_H
 #define COMPUTEPIXEL_H
 
+#ifdef __cplusplus
 extern "C"
 {
-    double calculatePixel(const double x_0, const double y_0, const unsigned maxIterations);
-}
-
 #endif
+    double calculatePixel(const double x_0, const double y_0, const unsigned maxIterations);
+#ifdef __cplusplus
+}
+#endif
+#endif // COMPUTEPIXEL_H
